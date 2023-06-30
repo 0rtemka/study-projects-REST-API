dec4fdd87611662d6fe2464e4fa01bbf5a460125
0rtemka <pepussi@yandex.ru>
2023-06-30 18:31:29 +0500

    Initial commit

---
+++ src/main/java/com/example/studyprojects/StudyProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProjectsApplication.class, args);
+    }
+
+}
